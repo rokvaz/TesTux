c687f072f88d9d2420102a668a8c2c746736e9bc
telijus <linasjakutis28@gmail.com>
2024-03-06 10:48:02 +0200

    Įdiegtas leaderboard puslapis

---
+++ frontend/src/pages/leaderboard.tsx
@@ -0,0 +1,8 @@
+import '../app/globals.css';
+
+export default function Leaderboard() {
+    return (
+        <div className="bg-custom-aqua min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center mb-3">LeaderBoard</h1>
+            </div>);
+}
