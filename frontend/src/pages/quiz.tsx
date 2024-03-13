4e85fa73e43f00fc2b7a4382f59b94f52746134e
Serious-Fin <augustasgri@gmail.com>
2024-03-13 18:59:25 +0200

    create score logic and game over visuals

--- frontend/src/pages/quiz.tsx
+++ frontend/src/pages/quiz.tsx
@@ -44,6 +44,7 @@ const jsonResponse: ResponseQuestion [] = [
 interface Answer {
   text: string | number;
   isCorrect: boolean;
+  points: number;
 }
 interface Question {
   question: string;
@@ -61,11 +62,11 @@ interface ResponseQuestion {
   neteising_ats3: string | number;
 }
 
-
-
 export default function Quiz() {
 
   const [currentQuestion, setCurrentQuestion] = useState<Question | null>(null);
+  const [score, setScore] = useState(0);
+  const [bgColor, setBgColor] = useState("bg-custom-aqua");
 
   let shuffleAnswers = (array: Answer[]) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -77,6 +78,26 @@ export default function Quiz() {
     return array;
   }
 
+  let clickHandler = (answer: Answer) => {
+    if (answer.isCorrect) {
+      // load other question
+      // increase score
+      //setScore(() => score + answer.points)
+      const timer = setTimeout(() => {
+        setScore(() => score + answer.points)
+      }, 500)
+    } else { // Wrong answer picked -> game over
+      const timer = setTimeout(() => {
+        setBgColor("bg-red-500") // Change background to red
+        document.getElementById("box")!.innerHTML="Game over" // Write "game over" in question box
+        document.getElementById("choice")!.innerHTML="" // Delete all buttons
+        const timer = setTimeout(() => { // Redirect to gameover page after 1.5 sec
+          window.location.href = 'http://localhost:3000/gameover';
+        }, 1500);
+      }, 500)
+    }
+  }
+
   useEffect(() => {
     // Function to pick a random question
     const pickRandomQuestion = () => {
@@ -85,15 +106,15 @@ export default function Quiz() {
       let answers: Answer[];
       if (randomQuestionObject.tipas == 2) {
         answers = [
-          {text: randomQuestionObject.teising_ats, isCorrect: true},
-          {text: randomQuestionObject.neteising_ats1, isCorrect: false}
+          {text: randomQuestionObject.teising_ats, isCorrect: true, points: randomQuestionObject.taskai},
+          {text: randomQuestionObject.neteising_ats1, isCorrect: false, points: randomQuestionObject.taskai}
         ]
       } else if (randomQuestionObject.tipas == 4) {
         answers = [
-          {text: randomQuestionObject.teising_ats, isCorrect: true},
-          {text: randomQuestionObject.neteising_ats1, isCorrect: false},
-          {text: randomQuestionObject.neteising_ats2, isCorrect: false},
-          {text: randomQuestionObject.neteising_ats3, isCorrect: false},
+          {text: randomQuestionObject.teising_ats, isCorrect: true, points: randomQuestionObject.taskai},
+          {text: randomQuestionObject.neteising_ats1, isCorrect: false, points: randomQuestionObject.taskai},
+          {text: randomQuestionObject.neteising_ats2, isCorrect: false, points: randomQuestionObject.taskai},
+          {text: randomQuestionObject.neteising_ats3, isCorrect: false, points: randomQuestionObject.taskai},
         ]
       } else {
         throw new Error("Incorrect question type. Legal types are 2 and 4");
@@ -112,12 +133,20 @@ export default function Quiz() {
     };
 
     pickRandomQuestion();
-  }, []);
+  }, [score]);
+
+  useEffect(() => {
+  }, [bgColor])
 
   if (!currentQuestion) return <div>Loading question...</div>;
 
   return (
-    <div className="bg-custom-aqua min-h-screen flex flex-col items-center justify-center">
+    <div className={`${bgColor} min-h-screen flex flex-col items-center justify-center`}>
+      {/* Score */}
+      <div className="text-3xl font-bold mb-8 p-4">
+        Score: {score}
+      </div>
+
       {/* Timer at the top */}
       <div className="text-5xl font-bold mb-8 p-4">
         30
@@ -125,30 +154,30 @@ export default function Quiz() {
 
       {/* Question section */}
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-2xl w-full mb-8 text-center">
-        <h2 className="text-2xl font-bold text-gray-800">{currentQuestion.question}</h2>
+        <h2 className="text-2xl font-bold text-gray-800" id="box">{currentQuestion.question}</h2>
       </div>
 
       {(currentQuestion.questionType == 4) ? (
-        <div className="grid grid-cols-2 gap-4 max-w-xs w-full">
-          <button className="bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90">
+        <div className="grid grid-cols-2 gap-4 max-w-xs w-full" id="choice">
+          <button className={`bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90 ${currentQuestion.answers[0].isCorrect ? "active:bg-green-500" : "active:bg-red-500"}`} onClick={() => {clickHandler(currentQuestion.answers[0])}}>
             {currentQuestion.answers[0].text}
           </button>
-          <button className="bg-custom-orange p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90">
+          <button className={`bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90 ${currentQuestion.answers[1].isCorrect ? "active:bg-green-500" : "active:bg-red-500"}`} onClick={() => {clickHandler(currentQuestion.answers[1])}}>
             {currentQuestion.answers[1].text}
           </button>
-          <button className="bg-custom-orange p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90">
+          <button className={`bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90 ${currentQuestion.answers[2].isCorrect ? "active:bg-green-500" : "active:bg-red-500"}`} onClick={() => {clickHandler(currentQuestion.answers[2])}}>
             {currentQuestion.answers[2].text}
           </button>
-          <button className="bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90">
+          <button className={`bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90 ${currentQuestion.answers[3].isCorrect ? "active:bg-green-500" : "active:bg-red-500"}`} onClick={() => {clickHandler(currentQuestion.answers[3])}}>
             {currentQuestion.answers[3].text}
           </button>
         </div>
       ) : (
-        <div className="grid grid-cols-2 gap-4 max-w-xs w-full">
-          <button className="bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90">
+        <div className="grid grid-cols-2 gap-4 max-w-xs w-full" id="choice">
+          <button className={`bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90 ${currentQuestion.answers[0].isCorrect ? "active:bg-green-500" : "active:bg-red-500"}`} onClick={() => {clickHandler(currentQuestion.answers[0])}}>
             {currentQuestion.answers[0].text}
           </button>
-          <button className="bg-custom-orange p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90">
+          <button className={`bg-custom-brown p-4 text-white font-bold rounded-lg shadow-md transform transition duration-150 ease-in-out hover:scale-105 active:scale-90 ${currentQuestion.answers[1].isCorrect ? "active:bg-green-500" : "active:bg-red-500"}`} onClick={() => {clickHandler(currentQuestion.answers[1])}}>
             {currentQuestion.answers[1].text}
           </button>
         </div>
